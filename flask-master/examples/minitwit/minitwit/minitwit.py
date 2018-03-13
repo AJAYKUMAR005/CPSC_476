22d8c428030458b5e5e21f305ff9c52e2843f79d
Thomas Ngo <tngo0508@gmail.com>
2018-03-13 16:49:36 -0700

    clean code after seperating mt_api and minitwit

--- flask-master/examples/minitwit/minitwit/minitwit.py
+++ flask-master/examples/minitwit/minitwit/minitwit.py
@@ -31,54 +31,6 @@ app.config.from_object(__name__)
 app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
 
 
-# def get_db():
-#     """Opens a new database connection if there is none yet for the
-#     current application context.
-#     """
-#     top = _app_ctx_stack.top
-#     if not hasattr(top, 'sqlite_db'):
-#         top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-#         top.sqlite_db.row_factory = sqlite3.Row
-#     return top.sqlite_db
-#
-#
-# @app.teardown_appcontext
-# def close_database(exception):
-#     """Closes the database again at the end of the request."""
-#     top = _app_ctx_stack.top
-#     if hasattr(top, 'sqlite_db'):
-#         top.sqlite_db.close()
-#
-#
-# def init_db():
-#     """Initializes the database."""
-#     db = get_db()
-#     with app.open_resource('schema.sql', mode='r') as f:
-#         db.cursor().executescript(f.read())
-#     db.commit()
-#
-#
-# @app.cli.command('initdb')
-# def initdb_command():
-#     """Creates the database tables."""
-#     init_db()
-#     print('Initialized the database.')
-#
-#
-# def query_db(query, args=(), one=False):
-#     """Queries the database and returns a list of dictionaries."""
-#     cur = get_db().execute(query, args)
-#     rv = cur.fetchall()
-#     return (rv[0] if rv else None) if one else rv
-#
-#
-# def get_user_id(username):
-#     """Convenience method to look up the id for a username."""
-#     rv = query_db('select user_id from user where username = ?',
-#                   [username], one=True)
-#     return rv[0] if rv else None
-
-
 def format_datetime(timestamp):
     """Format a timestamp for display."""
     return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d @ %H:%M')
@@ -120,10 +72,6 @@ def timeline():
 @app.route('/public')
 def public_timeline():
     """Displays the latest messages of all users."""
-    # return render_template('timeline.html', messages=query_db('''
-    #     select message.*, user.* from message, user
-    #     where message.author_id = user.user_id
-    #     order by message.pub_date desc limit ?''', [PER_PAGE]))
     payload = {}
     url = 'http://127.0.0.1:5001/timeline'
     r = requests.get(url, json=payload)
@@ -133,8 +81,6 @@ def public_timeline():
 @app.route('/<username>')
 def user_timeline(username):
     """Display's a users tweets."""
-    # profile_user = query_db('select * from user where username = ?',
-    #                         [username], one=True)
     payload = {'username': username}
     url = 'http://127.0.0.1:5001/users/' + username
     r = requests.get(url, json=payload)
@@ -144,19 +90,9 @@ def user_timeline(username):
     followed = False
     if g.user:
         payload = {'user_id': session['user_id'], 'pw_hash': session['pw_hash'], 'username': session['username'], 'profile_user_id': profile_user['user_id']}
-        # followed = query_db('''select 1 from follower where
-        #     follower.who_id = ? and follower.whom_id = ?''',
-        #     [session['user_id'], profile_user['user_id']],
-        #     one=True) is not None
         url = 'http://127.0.0.1:5001/users/' + str(session['user_id']) + '/follow'
         r = requests.get(url, json=payload)
         followed = r.json() is not None
-    # return render_template('timeline.html', messages=query_db('''
-    #         select message.*, user.* from message, user where
-    #         user.user_id = message.author_id and user.user_id = ?
-    #         order by message.pub_date desc limit ?''',
-    #         [profile_user['user_id'], PER_PAGE]), followed=followed,
-    #         profile_user=profile_user)
     payload = {'user_id': session['user_id'], 'pw_hash': session['pw_hash'], 'username': session['username'], 'profile_user_id': profile_user['user_id']}
     url = 'http://127.0.0.1:5001/users/' + profile_user['username'] + '/messages'
     r = requests.get(url, json=payload)
@@ -173,10 +109,6 @@ def follow_user(username):
     payload = {'user_id': session['user_id'], 'pw_hash': session['pw_hash'], 'username': session['username'], 'whom_id': whom_id}
     url = 'http://127.0.0.1:5001/users/' + str(session['user_id']) + '/add_follow'
     r = requests.post(url, json=payload)
-    # db = get_db()
-    # db.execute('insert into follower (who_id, whom_id) values (?, ?)',
-    #           [session['user_id'], whom_id])
-    # db.commit()
     flash('You are now following "%s"' % username)
     return redirect(url_for('user_timeline', username=username))
 
@@ -192,10 +124,6 @@ def unfollow_user(username):
     payload = {'user_id': session['user_id'], 'pw_hash': session['pw_hash'], 'username': session['username'], 'whom_id': whom_id}
     url = 'http://127.0.0.1:5001/users/' + str(session['user_id']) + '/unfollow'
     r = requests.delete(url, json=payload)
-    # db = get_db()
-    # db.execute('delete from follower where who_id=? and whom_id=?',
-    #           [session['user_id'], whom_id])
-    # db.commit()
     flash('You are no longer following "%s"' % username)
     return redirect(url_for('user_timeline', username=username))
 
@@ -209,11 +137,6 @@ def add_message():
         payload = {'author_id': session['user_id'], 'pw_hash': session['pw_hash'], 'username': session['username'], 'text': request.form['text']}
         url = 'http://127.0.0.1:5001/users/' + session['username'] + '/add_message'
         r = requests.post(url, json=payload)
-        # db = get_db()
-        # db.execute('''insert into message (author_id, text, pub_date)
-        #   values (?, ?, ?)''', (session['user_id'], request.form['text'],
-        #                         int(time.time())))
-        # db.commit()
         flash('Your message was recorded')
     return redirect(url_for('timeline'))
 
@@ -225,8 +148,6 @@ def login():
         return redirect(url_for('timeline'))
     error = None
     if request.method == 'POST':
-        # user = query_db('''select * from user where
-        #     username = ?''', [request.form['username']], one=True)
         payload = {'username': request.form['username']}
         print request.form['username']
         print payload
@@ -271,12 +192,6 @@ def register():
             print payload
             url = 'http://127.0.0.1:5001/users/Sign_up'
             r = requests.post(url, json=payload)
-            # db = get_db()
-            # db.execute('''insert into user (
-            #   username, email, pw_hash) values (?, ?, ?)''',
-            #   [request.form['username'], request.form['email'],
-            #    generate_password_hash(request.form['password'])])
-            # db.commit()
             flash('You were successfully registered and can login now')
             return redirect(url_for('login'))
     return render_template('register.html', error=error)
